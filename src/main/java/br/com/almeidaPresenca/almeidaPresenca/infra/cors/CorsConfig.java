5e1dfc9c0b4d0cf998e6cedef6d6a10d3cd1f9e2
Caio Marins <marinsroyale@gmail.com>
2025-05-05 02:46:01 -0300

    fix link front

--- src/main/java/br/com/almeidaPresenca/almeidaPresenca/infra/cors/CorsConfig.java
+++ src/main/java/br/com/almeidaPresenca/almeidaPresenca/infra/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // todos os endpoints
-                .allowedOrigins("http://localhost:4200") // permite o frontend
+                .allowedOrigins("https://almeidatucuruvi.vercel.app/") // permite o frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
